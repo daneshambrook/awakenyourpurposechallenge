96ebf87d0c4074acb8420186aacf1b62d6c138f8
daneshambrook <daneshambrook@gmail.com>
2024-11-27 20:46:42 -0800

    Update wheelOfLife.js

--- wheelOfLife.js
+++ wheelOfLife.js
@@ -1,25 +1,3 @@
-/**
- * The MIT License (MIT)
- * Copyright (c) 2023 Matthias Kappeller
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 !(function (t, e) {
   "object" == typeof exports && "undefined" != typeof module
     ? (module.exports = e())
@@ -32,7 +10,7 @@
   "use strict";
   return {
     setup: function (chartEl, sectionsEl, config) {
-      const chartSeries = Array(config.length).fill(10);
+      const chartSeries = Array(config.length).fill(1); // Updated to start at 1
       const chartLabels = config.map((c) => c.name);
       const chartColors = config.map((c) => c.color);
 
@@ -93,7 +71,7 @@
         input.min = "1";
         input.max = "10";
         input.step = "1";
-        input.value = "10";
+        input.value = "1"; // Updated to start at 1
         input.oninput = (e) => {
           chartSeries[i] = e.target.value;
           chart.updateSeries(chartSeries, false);
